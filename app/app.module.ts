b90790b7cb6e070b26e72baebf1d2d21b81ea58c
wrayta <wrayta@hotmail.com>
2016-12-01 12:24:29 -0500

    End of tutorial; HTTP phase

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// Imports for loading and configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import './rxjs-extensions';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,13 +21,16 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [ 
 	BrowserModule,
 	FormsModule,
+	HttpModule,
+	InMemoryWebApiModule.forRoot(InMemoryDataService),
 	AppRoutingModule
   ],
   declarations: [ 
 	AppComponent, 
 	DashboardComponent,
 	HeroDetailComponent,
-	HeroesComponent
+	HeroesComponent,
+	HeroSearchComponent
   ],
   providers: [
 	HeroService
