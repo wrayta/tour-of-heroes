4d529d8c24c0ce647fdd9aa827f78a6c6e58fe4c
wrayta <wrayta@hotmail.com>
2016-12-01 01:37:52 -0500

    Initial commit before HTTP

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  styleUrls: [ 'app.component.css' ],
+  template: `
+	<h1>{{title}}</h1>
+	<nav>
+		<a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+		<a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+	</nav>
+	<router-outlet></router-outlet>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,32 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+	BrowserModule,
+	FormsModule,
+	AppRoutingModule
+  ],
+  declarations: [ 
+	AppComponent, 
+	DashboardComponent,
+	HeroDetailComponent,
+	HeroesComponent
+  ],
+  providers: [
+	HeroService
+  ],
+  bootstrap: [ 
+	AppComponent 
+  ]
 })
 export class AppModule { }
